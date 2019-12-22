b2c7304e70cec6d316dc9a60ec5eff0a9c223151
chris johnston <chris@johnstoncode.com>
2019-12-22 18:33:49 +0000

    added 2019 day 21 and part complete day 22

---
+++ 2019/day_21/part_2.php
@@ -0,0 +1,145 @@
+<?php
+
+$input = file_get_contents(__DIR__ . '/input.txt');
+$codes = explode(',', $input);
+
+$inst = "NOT A T\nNOT B J\nOR J T\nNOT C J\nOR J T\nNOT D J\nNOT J J\nAND T J\nNOT E T\nNOT T T\nOR H T\nAND T J\nRUN\n";
+$aInst = [];
+
+foreach (str_split($inst) as $char) {
+    $aInst[] = ord($char);
+}
+
+$computer = new Computer($codes, $aInst);
+
+while ($output = $computer->run()) {
+    if ($output > 255) {
+        echo $output . PHP_EOL;
+        break;
+    }
+}
+
+
+class Computer
+{
+    public $codes;
+    private $inputs;
+    private $output;
+    private $relativeBase;
+    private $i = 0;
+
+    public function __construct(array $codes, array $inputs)
+    {
+        $this->codes = $codes;
+        $this->inputs = $inputs;
+        $this->output = null;
+        $this->relativeBase = 0;
+    }
+
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    public function getParameterModes($code)
+    {
+        $code = str_pad($code, 5, '0', STR_PAD_LEFT);
+        $opcode = (int) substr($code, 3, 2);
+        $param1 = (int) substr($code, 2, 1);
+        $param2 = (int) substr($code, 1, 1);
+        $param3 = (int) substr($code, 0, 1);
+
+        return [$opcode, $param1, $param2, $param3];
+    }
+
+    public function getParameterValue(int $mode, int $position): int
+    {
+        switch ($mode) {
+            case 0:
+                return $this->codes[$this->codes[$position]] ?? 0;
+            case 1:
+                return $this->codes[$position] ?? 0;
+            case 2:
+                $memoryAddress = $this->relativeBase + $this->codes[$position];
+                return $this->codes[$memoryAddress] ?? 0;
+        }
+    }
+
+    public function setValue(int $index, int $value, int $mode)
+    {
+        if ($mode === 0) {
+            $this->codes[$index] = $value;
+        } else if ($mode === 2) {
+            $this->codes[$this->relativeBase + $index] = $value;
+        }
+    }
+
+    public function run()
+    {
+        for (; $this->i < count($this->codes); $this->i+=0) {
+            [$opcode, $param1, $param2, $param3] = $this->getParameterModes($this->codes[$this->i]);
+        
+            switch ($opcode) {
+                case 1:
+                    $result = $this->getParameterValue($param1, $this->i + 1) + $this->getParameterValue($param2, $this->i + 2);
+                    $this->setValue($this->codes[$this->i + 3], $result, $param3);
+                    $this->i += 4;
+                    break;
+                case 2:
+                    $result = $this->getParameterValue($param1, $this->i + 1) * $this->getParameterValue($param2, $this->i + 2);
+                    $this->setValue($this->codes[$this->i + 3], $result, $param3);
+                    $this->i += 4;
+                    break;
+                case 3:
+                    $this->setValue($this->codes[$this->i + 1], array_shift($this->inputs), $param1);
+                    $this->i += 2;
+                    break;
+                case 4:
+                    $this->output = $this->getParameterValue($param1, $this->i + 1);
+                    $this->i += 2;
+                    return $this->output;
+                case 5:
+                    if ($this->getParameterValue($param1, $this->i + 1) > 0) {
+                        $this->i = $this->getParameterValue($param2, $this->i + 2);
+                        break;
+                    }
+
+                    $this->i += 3;
+                    break;
+                case 6:
+                    if ($this->getParameterValue($param1, $this->i + 1) === 0) {
+                        $this->i = $this->getParameterValue($param2, $this->i + 2);
+                        break;
+                    }
+
+                    $this->i += 3;
+                    break;
+                case 7:
+                    $result = $this->getParameterValue($param1, $this->i + 1) < $this->getParameterValue($param2, $this->i + 2) ? 1 : 0;
+                    $this->setValue($this->codes[$this->i + 3], $result, $param3);
+
+                    $this->i += 4;
+                    break;
+                
+                    break;
+                case 8:
+                    $result = $this->getParameterValue($param1, $this->i + 1) === $this->getParameterValue($param2, $this->i + 2) ? 1 : 0;
+                    $this->setValue($this->codes[$this->i + 3], $result, $param3);
+
+                    $this->i += 4;
+                    break;
+                case 9:
+                    $this->relativeBase += $this->getParameterValue($param1, $this->i + 1);
+                    $this->i += 2;
+                    break;
+                case 99:
+                    break 2;
+                default:
+                    var_dump($this->i);
+                    var_dump($this->codes[$this->i]);
+                    var_dump([$opcode, $param1, $param2, $param3]);
+                    die();
+            }
+        }
+    }
+}

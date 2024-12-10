53a4c3bfe9090c124702520d55a75e47af4fe957
Chris Johnston <chris.johnston@sport80.com>
2024-12-10 17:51:04 +0000

    add 2024 day 7

---
+++ 2024/day_07/part2.php
@@ -0,0 +1,36 @@
+<?php
+
+$rawInput = file_get_contents(__DIR__ . '/input.txt');
+
+$calibrations = [];
+
+foreach (explode("\n", $rawInput) as $line) {
+    [$result, $nums] = explode(": ", $line);
+    $calibrations[] = [(int) $result, ...array_map(fn ($num) => (int) $num, explode(' ', $nums))];
+}
+
+function solve(int $total, array $nums): bool
+{
+    if (count($nums) === 1) {
+        return $nums[0] === $total;
+    }
+
+    $a = array_shift($nums);
+    $b = array_shift($nums);
+    $add = $a + $b;
+    $mul = $a * $b;
+    $con = (int) ($a . $b);
+
+    return solve($total, [$add, ...$nums]) || solve($total, [$mul, ...$nums]) || solve($total, [$con, ...$nums]);
+}
+
+$total = 0;
+
+foreach ($calibrations as $calibration) {
+    $result = array_shift($calibration);
+    if (solve($result, $calibration)) {
+        $total += $result;
+    }
+}
+
+var_dump($total);

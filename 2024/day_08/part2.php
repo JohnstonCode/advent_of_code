4c783f8930c8edc8194107522d052b377169f3fb
Chris Johnston <chris.johnston@sport80.com>
2024-12-11 20:17:09 +0000

    add 2024 day 8

---
+++ 2024/day_08/part2.php
@@ -0,0 +1,59 @@
+<?php
+
+$rawInput = file_get_contents(__DIR__ . '/input.txt');
+
+$map = [];
+$antennas = [];
+
+foreach (explode("\n", $rawInput) as $y => $line) {
+    foreach (str_split($line) as $x => $char) {
+        $map[$y][$x] = $char;
+
+        if (preg_match('/[a-zA-Z0-9]/', $char)) {
+            $antennas[] = [$char, $x, $y];
+        }
+    }
+}
+
+$locations = [];
+
+for ($i = 0; $i < count($antennas); $i++) {
+    for ($j = 0; $j < count($antennas); $j++) {
+        $a = $antennas[$i];
+        $b = $antennas[$j];
+
+        if ($a[0] !== $b[0]) {
+            continue;
+        }
+
+        $diff = [$a[1] - $b[1], $a[2] - $b[2]];
+        $pos = [$a[1] + $diff[0], $a[2] + $diff[1]];
+        $neg = [$b[1] - $diff[0], $b[2] - $diff[1]];
+
+        if (isset($map[$pos[1]][$pos[0]])) {
+            $locations[] = sprintf('%s,%s', $pos[0], $pos[1]);
+        }
+
+        if (isset($map[$neg[1]][$neg[0]])) {
+            $locations[] = sprintf('%s,%s', $neg[0], $neg[1]);
+        }
+
+        if ($i === $j) {
+            continue;
+        }
+
+        while (isset($map[$pos[1]][$pos[0]])) {
+            $locations[] = sprintf('%s,%s', $pos[0], $pos[1]);
+            $pos[0] += $diff[0];
+            $pos[1] += $diff[1];
+        }
+
+        while (isset($map[$neg[1]][$neg[0]])) {
+            $locations[] = sprintf('%s,%s', $neg[0], $neg[1]);
+            $neg[0] -= $diff[0];
+            $neg[1] -= $diff[1];
+        }
+    }
+}
+
+var_dump(count(array_unique($locations)));
